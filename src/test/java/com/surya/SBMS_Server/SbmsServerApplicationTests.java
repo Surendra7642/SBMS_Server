945719ccf01c343db375e098b6adf1e5a3b484fb
Surendra7642 <komarasurendra76@gmail.com>
2023-09-25 19:22:23 +0530

    Service Registry(eureka server)

---
+++ src/test/java/com/surya/SBMS_Server/SbmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surya.SBMS_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
